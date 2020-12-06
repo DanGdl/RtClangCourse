7b6aa01e316c02e5e5e2f8b6a2f2dd0268cde986
max <dangladste@gmail.com>
2020-12-06 19:32:18 +0200

    # merge

--- RtServer/src/network.c
+++ RtServer/src/network.c
@@ -1,4 +1,4 @@
-/*
+/**
  * network.c
  *
  *  Created on: Nov 28, 2020
@@ -21,54 +21,11 @@
 #include "network.h"
 #include "errors.h"
 
-int network_open_client_socket_by_ip(const char *ip, const int port) {
-	int socket_descriptor = socket(PF_INET, SOCK_STREAM, 0);
-	if (socket_descriptor == -1) {
-		log_error("Create socket failed");
-	}
-
-	struct sockaddr_in si;
-	memset(&si, 0, sizeof(si));
-	si.sin_family = PF_INET;
-	si.sin_addr.s_addr = inet_addr(ip);
-	si.sin_port = htons(port);
-	const int c = connect(socket_descriptor, (struct sockaddr*) &si, sizeof(si));
-	if (c == -1) {
-		return -1;
-	}
-	return socket_descriptor;
-}
-
-int network_open_client_socket(const char *address, const char *port) {
-	struct addrinfo hints;
-	struct addrinfo *res;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = PF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-
-	if (getaddrinfo(address, port, &hints, &res) == -1) {
-		log_error("Can't resolve address");
-	}
-	const int socket_descriptor = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
-	if (socket_descriptor == -1) {
-		log_error("Create socket failed");
-	}
-	const int c = connect(socket_descriptor, res -> ai_addr, res -> ai_addrlen);
-	freeaddrinfo(res);
-	if (c == -1) {
-		log_error("Can't connect");
-	}
-	return socket_descriptor;
-}
 
 void network_close_socket(const int *socket_descriptor) {
 	if (socket_descriptor != NULL && *socket_descriptor != 0) {
 		close(*socket_descriptor);
 	}
-}
-
-int network_send(const int *socket_descriptor, uint8_t *message, const int length) {
-	return send(*socket_descriptor, message, length, 0);
 }
 
 int network_receive(const int *socket_descriptor, uint8_t *buf, int buffer_size) {
@@ -85,71 +42,6 @@ int network_receive(const int *socket_descriptor, uint8_t *buf, int buffer_size)
 	}
 	while (bytes_received);
 	return total_received;
-}
-
-int get_str_data_for_interface(const char *name, char **network_data) {
-	struct ifaddrs *addresses;
-	const int result = getifaddrs(&addresses);
-	if (result == -1) {
-		return -1;
-	}
-
-	for (struct ifaddrs *ifa = addresses; ifa != NULL; ifa = ifa -> ifa_next) {
-		if (ifa -> ifa_addr == NULL || strcmp(name, ifa -> ifa_name) != 0) {
-			continue;
-		}
-		if (ifa -> ifa_addr -> sa_family == AF_INET) {
-
-		   struct sockaddr_in *sa = (struct sockaddr_in *) ifa -> ifa_addr;
-		   char *value = inet_ntoa(sa -> sin_addr);
-		   strcpy(network_data[0], value);
-
-		   sa = (struct sockaddr_in *) ifa -> ifa_netmask;
-		   value = inet_ntoa(sa -> sin_addr);
-		   strcpy(network_data[1], value);
-		}
-		if (ifa -> ifa_addr -> sa_family == AF_PACKET) {
-			 struct sockaddr_ll *s = (struct sockaddr_ll*) ifa -> ifa_addr;
-			 for (int i = 0; i < (s -> sll_halen); i++) {
-				 if (i + 1 == s -> sll_halen) {
-					 sprintf(&(network_data[2][i * 3]), "%02x", s -> sll_addr[i]);
-				 } else {
-					 sprintf(&(network_data[2][i * 3]), "%02x%c", s -> sll_addr[i], ':');
-				 }
-			 }
-		}
-	}
-	freeifaddrs(addresses);
-	return 0;
-}
-
-int get_data_for_interface(const char *name, AddressData *network_data) {
-	struct ifaddrs *addresses;
-	const int result = getifaddrs(&addresses);
-	if (result == -1) {
-		return -1;
-	}
-
-	for (struct ifaddrs *ifa = addresses; ifa != NULL; ifa = ifa -> ifa_next) {
-		if (ifa -> ifa_addr == NULL || strcmp(name, ifa -> ifa_name) != 0) {
-			continue;
-		}
-		if (ifa -> ifa_addr -> sa_family == AF_INET) {
-		   struct sockaddr_in *sa = (struct sockaddr_in *) ifa -> ifa_addr;
-		   memcpy(&(network_data -> ip), &(sa -> sin_addr.s_addr), sizeof(uint32_t));
-
-		   sa = (struct sockaddr_in *) ifa -> ifa_netmask;
-		   memcpy(&(network_data -> mask), &(sa -> sin_addr.s_addr), sizeof(uint32_t));
-		}
-		if (ifa -> ifa_addr -> sa_family == AF_PACKET) {
-			 struct sockaddr_ll *s = (struct sockaddr_ll*) ifa -> ifa_addr;
-			 for (int i = 0; i < (s -> sll_halen); i++) {
-				memcpy(&(network_data -> mac[i]), &(s -> sll_addr[i]), sizeof(unsigned char));
-			 }
-		}
-	}
-	freeifaddrs(addresses);
-	return 0;
 }
 
 crc calculate_crc(uint8_t const message[], int nBytes) {
